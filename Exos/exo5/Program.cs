adcb986d9d093095ba9d757eec91d9a3c5e8ed8f
Ernestas.Cernys <ecernys@gmail.com>
2021-04-15 10:27:43 +0200

    exo 1-6

---
+++ Exos/exo5/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo5
+{
+    class Program
+    {
+        static bool IsPrime(int value)
+        {
+            if (value > 0)
+            {
+                double sqrt = Math.Sqrt(value);
+                int n = (int)Math.Floor(sqrt) + 1;
+
+                for (int i = 2; i < n; i++)
+                {
+                    if (value % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        static void DisplayPrimes()
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            DisplayPrimes();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exos/exo6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exo6
+{
+    class Program
+    {
+        static int Gcd(int a, int b)
+        {
+            int sup = a;
+            int inf = b;
+
+            if (b > a)
+            {
+                sup = b;
+                inf = a;
+            }
+            
+            if (inf != 0 && sup % inf != 0)
+            {
+                sup = inf;
+                inf = sup / inf;
+                return Gcd(sup, inf);
+            }
+            else
+            {
+                return inf;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gcd(100, 50));
+            Console.WriteLine(Gcd(50, 100));
+            Console.WriteLine(Gcd(3, 7));
+            Console.WriteLine(Gcd(1, 5));
+            Console.WriteLine(Gcd(1000, 13));
+            Console.ReadKey();
+        }
+    }
+}
